097628f02f29564857fb87a0e1973c3d41d090d8
poncerapha <raphafleury123@gmail.com>
2019-02-14 12:34:22 -0200

    New Rocks

--- Haunted Zombie Rush/Assets/Scripts/PlatformMoviment.cs
+++ Haunted Zombie Rush/Assets/Scripts/PlatformMoviment.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class PlatformMoviment : MonoBehaviour {
 
     [SerializeField] private float speed = 1;
-    private float resetPosition = 82.93f;
+    [SerializeField] float resetPosition = 82.9f;
+    [SerializeField] float startPosition = 0f;
 
     // Use this for initialization
     void Start () {
@@ -13,12 +14,12 @@ public class PlatformMoviment : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	protected virtual void Update () {
         transform.Translate(Vector3.right * (speed * Time.deltaTime));
 
         if(transform.localPosition.x >= resetPosition)
         {
-            Vector3 newPos = new Vector3(0, transform.position.y, transform.position.z);
+            Vector3 newPos = new Vector3(startPosition, transform.position.y, transform.position.z);
             transform.position = newPos;
         }
 		
--- Haunted Zombie Rush/Assets/Scripts/Rock.cs
+++ Haunted Zombie Rush/Assets/Scripts/Rock.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rock : MonoBehaviour {
+public class Rock : PlatformMoviment 
+{
 
     [SerializeField] Vector3 topPosition;
     [SerializeField] Vector3 bottomPosition;
@@ -12,8 +13,13 @@ public class Rock : MonoBehaviour {
         StartCoroutine(Move(bottomPosition));
 		
 	}
-	
-	IEnumerator Move(Vector3 target)
+
+    protected override void Update()
+    {
+        base.Update();
+    }
+
+    IEnumerator Move(Vector3 target)
     {
         while(Mathf.Abs((target - transform.localPosition).y) > 0.20f) {
             Vector3 direction = target.y == topPosition.y ? Vector3.up : Vector3.down;
